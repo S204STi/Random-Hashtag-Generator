a2e1d811a0dfdda598c579d7cf7d656455efaacf
Roy <rechase84@gmail.com>
2016-05-23 16:13:16 -0600

    have working converter for user defined string

--- src.js
+++ src.js
@@ -32,25 +32,53 @@ var bool = true;//I only want the user to be able to run this once
 var myData;
 function generateText(err, data){
 	if(bool){
-		//console.log(data.text_out);
 		myData = data;
-		//console.log(myData.text_out);
 		var newEl = document.createElement('p');
 		var text = data.text_out;
-		console.log('text', text);
 		//because of what the API returns, I need to pull ul and li elements from text
 		var newText = text.toLowerCase().match(/[\w]+/g);
-		console.log('newText', newText);
 		var newStr = '';
 		var newArr = newText.slice(3,-3);
-		console.log('product of function to remove ul, li, r from text', newArr);
 		//need to concatenate this array into a string
 		for (var i = 0; i < newArr.length; i++){
 			newStr = newStr.concat(newArr[i]);
 		}
 		newEl.innerHTML = "#"+newStr;
-		console.log(newEl.innerHTML);
 		document.getElementById('result').appendChild(newEl);
 		bool = false;
 	}
 }
+
+
+//convert user-provided string to a useable string for the purposes of this website
+document.getElementById('convert').addEventListener('click', convertText);
+
+var otherBool = true;
+function convertText(){
+	if(otherBool){
+			var newEl = document.createElement('p');
+			var text = document.getElementById('input').value;
+			var newText = text.toLowerCase().match(/[\w]+/g);
+			var newStr = '';
+			for (var i = 0; i < newText.length; i++){
+				newStr = newStr.concat(newText[i]);
+			}
+			newEl.innerHTML = "#"+newStr;
+			document.getElementById('userResult').appendChild(newEl);
+			otherBool=false;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
