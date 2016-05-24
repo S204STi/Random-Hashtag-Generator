e53a8984d5277dd6483189fe09663094275dc22f
Roy <rechase84@gmail.com>
2016-05-24 15:17:42 -0600

    have working copy to clipboard, not mobile-friendly yet

--- src.js
+++ src.js
@@ -24,13 +24,13 @@ function ajax(method, url, handler, data) {
 
 //ajax call -----
 document.getElementById('create').addEventListener('click', function(){
-	ajax('GET', 'http://www.randomtext.me/api/gibberish/ul-1/20-25', generateText);
+	ajax('GET', 'http://www.randomtext.me/api/gibberish/ul-1/25-30', generateText);
 });
 //---------------
 
 var bool = true;//I only want the user to be able to run this once
 var myData;
-var randEl = '';
+var randEl = '';//to store string to be copied later
 function generateText(err, data){
 	if(bool){
 		myData = data;
@@ -40,7 +40,7 @@ function generateText(err, data){
 		var newText = text.toLowerCase().match(/[\w]+/g);
 		var newStr = '';
 		var newArr = newText.slice(3,-3);
-		//need to concatenate this array into a string preceeding each word with a "#"
+		//need to concatenate this array into a string, preceeding each word with a "#"
 			for (var i = 0; i < newArr.length; i++){
 				newStr = newStr.concat("#"+newArr[i]);
 			}
@@ -48,27 +48,21 @@ function generateText(err, data){
 		document.getElementById('result').appendChild(newEl);
 		randEl = newEl.innerHTML;
 		console.log('randEl', randEl);
-		// newEl.getElementsByTagName('p').addEventListener('click', randomCopy);
-		console.log('newEl elements', newEl);
 		bool = false;
 	}
 }
-//copy to clipboard functionality
-//on click to copy data
-document.getElementById('randCopy').addEventListener('click', randomCopy);
 
-function randomCopy(event){
-	console.log('entering randomCopy function');
-	event.clipboardData.setData('text/plain', randEl);
-	console.log('randEl', randEl);
-	event.preventDefault();
-	window.alert('Copied');
-	console.log('successfully fire copy event');
+document.getElementById('randCopy').addEventListener('click', copyToClipboard);
+
+function copyToClipboard() {
+  window.prompt("Copy to clipboard: Ctrl+C/Cmd+C, Enter", randEl);
 }
+
+
 
 //convert user-provided string to a useable string for the purposes of this website
 document.getElementById('convert').addEventListener('click', convertText);
-
+var userEl ='';
 var otherBool = true;
 function convertText(){
 	if(otherBool){
@@ -77,23 +71,33 @@ function convertText(){
 			var newText = text.toLowerCase().match(/[\w]+/g);
 			var newStr = '';
 			for (var i = 0; i < newText.length; i++){
-				newStr = newStr.concat(newText[i]);
+				newStr = newStr.concat("#" + newText[i]);
 			}
-			newEl.innerHTML = "#"+newStr;
-			console.log('newEl.innerHTML', newEl.innerHTML);
+			newEl.innerHTML = newStr;
+			userEl = newEl.innerHTML;
+			console.log('userEl', userEl);
 			document.getElementById('userResult').appendChild(newEl);
 			otherBool=false;
 		}
 	}
 
-//document.addEventListener('copy', function(e){
-//     e.clipboardData.setData('text/plain', 'Hello, world!');
-//     e.clipboardData.setData('text/html', '<b>Hello, world!</b>');
-//     e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
-// });
+document.getElementById('convCopy').addEventListener('click', copyUserToClipboard);
+
+function copyUserToClipboard(){
+	window.prompt("Copy to clipboard: Ctrl+C/Cmd+C, Enter", userEl);
+}
 
 
+//---code for showing hidden div elements ------
+document.getElementById('nav-link-a').addEventListener('click', showAbout);
 
+function showAbout(){
+	var newArticle = document.getElementById('aboutarticle');
+	newArticle.style.textAlign = 'center';
+	newArticle.style.padding = "5% 5%";
+
+	document.getElementsByClassName('site-wrapper-inner').appendChild(newArticle);
+}
 
 
 
